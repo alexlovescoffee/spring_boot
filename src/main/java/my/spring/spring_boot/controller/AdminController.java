b88059e01c7214f7945d25dbb76123fb936f30a1
Achkasov_alexey <alexeyfaker@gmail.com>
2021-10-27 11:35:51 +0300

    Fix RestController

--- src/main/java/my/spring/spring_boot/controller/AdminController.java
+++ src/main/java/my/spring/spring_boot/controller/AdminController.java
@@ -1,23 +1,15 @@
 package my.spring.spring_boot.controller;
 
-import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AllArgsConstructor;
 import my.spring.spring_boot.dto.UserDto;
-import my.spring.spring_boot.dto.jsonview.OnAdd;
-import my.spring.spring_boot.dto.jsonview.OnEdit;
-import my.spring.spring_boot.dto.jsonview.OnGet;
-import my.spring.spring_boot.exception.servicelevel.DataNotSavedException;
-import my.spring.spring_boot.model.User;
 import my.spring.spring_boot.service.UserService;
 import my.spring.spring_boot.service.UserDtoService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -44,66 +36,5 @@ public class AdminController {
                 .orElseThrow()
         );
         return "boot";
-    }
-
-    @GetMapping("get-user")
-    @JsonView(OnGet.class)
-    @ResponseBody
-    public ResponseEntity<UserDto> getUser(@RequestParam("id") long id) {
-        UserDto userDto = userDtoService.from_User_ToUserDto(userService.getUserById(id));
-        return ResponseEntity.ok(userDto);
-    }
-
-    @PostMapping
-    @ResponseBody
-    @JsonView(OnGet.class)
-    @Validated(OnAdd.class)
-    public ResponseEntity<?> addUser(@Valid @RequestBody @JsonView(OnAdd.class) final UserDto userDto) {
-        try {
-            User newUser = userDtoService.from_UserDto_ToUser(userDto);
-            userService.addUser(newUser);
-            return ResponseEntity.ok(userDtoService.from_User_ToUserDto(newUser));
-        } catch (DataNotSavedException e) {
-            return ResponseEntity.badRequest().body("Error! User not added! Message: " + e.getMessage());
-        }
-    }
-
-    @PutMapping
-    @ResponseBody
-    @JsonView(OnGet.class)
-    @Validated(OnEdit.class)
-    public ResponseEntity<?> updateUser(@Valid @RequestBody @JsonView(OnEdit.class) final UserDto userDto,
-                                        Authentication auth) {
-        try {
-            long currentUserId = userService.getUserByEmail(auth.getName()).getId();
-            User updateUser = userDtoService.from_UserDto_ToUser(userDto);
-            userService.updateUser(updateUser);
-
-            //если пользователь изменил себя, то произойдет logout
-            if (currentUserId == updateUser.getId()) {
-                auth.setAuthenticated(false);
-                return ResponseEntity.status(205).header("re-authenticate", "/logout").build();
-            }
-
-            return ResponseEntity.ok(userDtoService.from_User_ToUserDto(updateUser));
-        } catch (DataNotSavedException e) {
-            return ResponseEntity.badRequest().body("Error! User not updated! Message: " + e.getMessage());
-        }
-    }
-
-    @DeleteMapping
-    public ResponseEntity<?> deleteUser(@RequestParam("id") long id, Authentication auth) {
-        try {
-            long currentUserId = userService.getUserByEmail(auth.getName()).getId();
-            userService.deleteUserById(id);
-
-            //если пользователь удалил себя, то произойдет logout
-            if (currentUserId == id)
-                return ResponseEntity.status(205).header("re-authenticate", "/logout").build();
-
-            return ResponseEntity.ok(id);
-        } catch (DataNotSavedException e) {
-            return ResponseEntity.badRequest().body("Error! User not deleted! Message: " + e.getMessage());
-        }
     }
 }
